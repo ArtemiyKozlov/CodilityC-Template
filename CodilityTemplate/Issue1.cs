3a1cd80cbc4000949345e4855a23ae55566d6e67
Kozlov <ARKozlov@poland.luxoft.com>
2018-12-11 11:27:54 +0100

    Initial Commit

---
+++ CodilityTemplate/Issue1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility_template
+{
+    class Issue1
+    {
+        public int solution(int[] A)
+        {
+            return 0;
+        }
+    }
+}
---
+++ CodilityTemplate/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Codility_template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var issue = new Issue1();
+
+            var result = issue.solution(new[] { 1, 2, 3, 4, 5 });
+
+            Console.ReadLine();
+        }
+    }
+}
